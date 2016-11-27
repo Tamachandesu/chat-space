bcdf449a74b965b4a1d1ddd26b16a79dda1307c3
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2016-11-27 17:18:41 +0900

    Fixed position of "end"

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,18 @@
+require_relative 'boot'
+
 require 'rails/all'
 
 
 Bundler.require(*Rails.groups)
 
 module ChatSpace
+  
   class Application < Rails::Application
- 
-  end
     config.generators do |g|
       g.test_framework false
       g.helper false
       g.assets false
     end
-    
-    end
+
+  end
 end

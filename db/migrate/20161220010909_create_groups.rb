7e2f1428698473cfea9715a9861b174f0dd7577f
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2016-12-21 17:05:05 +0900

    Create group table

--- db/migrate/20161220010909_create_groups.rb
+++ db/migrate/20161220010909_create_groups.rb
@@ -1,7 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name
+      t.text :detail
+      t.references :user
+      t.references :message
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214060127) do
+ActiveRecord::Schema.define(version: 20161220010909) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.text     "detail",     limit: 65535
+    t.integer  "user_id"
+    t.integer  "message_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["message_id"], name: "index_groups_on_message_id", using: :btree
+    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false

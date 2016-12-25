e4e80b5d768e799eada6a8e699f22a5a4f947c79
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2016-12-26 00:04:44 +0900

    Install spec and factorygirl

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    nickname               "djfiojf"
+    email                  "fjidjf@fji.com"
+    password               "00000000"
+    password_confirmaiton  "00000000"
+  end
+end

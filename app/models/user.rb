4cfa30dffcc78349fa6b9ac7b447cb315bb2868d
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-05 21:08:09 +0900

    Make relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :messages
+  has_many :groups, through: :group_users
+  has_many :group_users
+
   validates :nickname, presence: true
 end

b0d2134c798159a0ad8338c3b5b6553cee788d6e
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-11 21:11:02 +0900

    Edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :groups, through: :group_users
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
 
   validates :nickname, presence: true
 end

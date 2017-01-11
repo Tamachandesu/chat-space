a8e78ab508c9e532c678f27c87ec3c285f4ad13b
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-11 13:27:18 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :messages, only: [:index]
-  resources :groups, only: [:new, :edit, :create]
+  root 'groups#index'
+  resources :groups, only: [:index, :new, :edit, :create, :update] do
+    resources :messages, only: [:index]
+  end
 end

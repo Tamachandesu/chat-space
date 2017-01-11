f930c68a57a318cda7a697bccc53284914e818a3
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-11 13:28:56 +0900

    Edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,37 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+    @user = current_user
+  end
 
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
     Group.create(group_params)
-    redirect_to controller: :messages, action: :index
+    redirect_to :root and return
   end
 
+  def edit
+  end
+
+  def update
+    @group.update(group_params)
+    redirect_to group_messages_path(@group)
+  end
 
   private
-  def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
 end
+
+

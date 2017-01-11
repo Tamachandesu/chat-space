163c9a5aed546818e152c62b989a6ed2af474e19
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-11 21:11:12 +0900

    Edit group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :group_users
-  has_many :group_users
-  accepts_nested_attributes_for :users
+  has_many :group_users, dependent: :destroy
+  accepts_nested_attributes_for :users, allow_destroy: true
 
   validates :name, presence: true
 end

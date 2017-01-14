de09c18e4fcdc6050e0d140bf69ac74a9f7a07e9
Tamachandesu <tamarutatsurou@tamarutatsurounoMacBook-Pro.local>
2017-01-14 11:30:16 +0900

    Edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
   def index
     @user = current_user
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group)
+    else
+      redirect_to group_messages_path(@group), notice: 'メッサージを入力してください'
+    end
+  end
+
+  private
+
+  def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end

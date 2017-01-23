6251fbdfc3fa88b76e02c8af4b0d04a121658c86
Tamachandesu <taturo.0324@gmail.com>
2017-01-23 16:28:18 +0900

    Edit messages_controller to post message asynchronously

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,10 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group)
+      respond_to do |format|
+        format.html {redirect_to group_messages_path(@group)}
+        format.json {render json: {message: {text: @message.text}}}
+      end
     else
       redirect_to group_messages_path(@group), notice: 'メッサージを入力してください'
     end

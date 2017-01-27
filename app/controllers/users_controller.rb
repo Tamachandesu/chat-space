3a67fca1cc62a4707db79876565604cc16f94dbe
Tamachandesu <taturo.0324@gmail.com>
2017-01-27 19:56:12 +0900

    Create uses_controller and make API

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.where('nickname LIKE ?', "%#{params[:nickname]}")
+
+    respond_to do |format|
+      format.json { render json: @users}
+    end
+  end
+end

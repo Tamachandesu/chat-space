447cbe00a4aba621ad0453bcaf4249a666e39ed1
Tamachandesu <taturo.0324@gmail.com>
2017-01-30 18:39:42 +0900

    Fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('nickname LIKE ?', "%#{params[:nickname]}")
-
+    @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
     respond_to do |format|
       format.json { render json: @users}
     end

655c9e5198ebac6b4f41ae971d4c4d7ea015b88e
Tamachandesu <taturo.0324@gmail.com>
2017-02-08 12:42:58 +0900

    Edit message.rb to connect wiz uploader

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  mount_uploader :image, ImageUploader
 
   validates :text, presence: true
+
+  def to_json
+    { name: user.nickname,
+      created_at: created_at,
+      body: body,
+      image: image.url }
+  end
 end

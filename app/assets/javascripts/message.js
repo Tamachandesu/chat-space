f0ed2d0e7d6c8da5545cbb57ed970d3fdcfcb9ef
Tamachandesu <taturo.0324@gmail.com>
2017-02-26 01:21:48 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,43 +3,71 @@ $(function() {
     var buildImage = "";
 
     if(message.image) {
-      buildImage = '<img class="chat-message__image" src= ' + message.image +'>'
-    } else {
-      buildImage = ""
+      buildImage = `<img class="chat-message__image" src="${message.image}" >`;
     }
-
-    var html = '<li class="chat-message">' +
-                '<div class="chat-message__header clearfix">' +
-                  '<p class="chat-message__name">' + message.nickname + '</p>' +
-                  '<p class="chat-message__time">' + message.created_at + '</p>' +
-                '</div>' +
-                '<p class = "chat-message__body">' + message.text + '<br>' + '</p>' +
-                'buildImage' +
-               '</li>'
+    var html =`
+            <li class="chat-message">
+              <div class="chat-message__header clearfix">
+                <p class="chat-message__name">${message.nickname}</p>
+                <p class="chat-message__time">${message.created_at}</p>
+              </div>
+              <p class = "chat-message__body">${message.text}<br></p>${buildImage}
+            </li>`;
     return html;
   }
 
-  $('#new-message').on('submit', function(e) {
+　function moveToBottom() {
+    $('.chat-body').animate({
+      scrollTop: $('.chat-messages').height() + $('.chat-message').height()
+    })
+  };
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var textField = $('#message_body');
+    $('#submit').removeAttr('data-disable-with');
     var formData = new FormData($("#new_message")[0]);
 
     $.ajax({
-      url: '/message.json',
+      type: 'POST',
+      url: './messages.json',
       data: formData,
-      method: 'POST',
+      dataType: 'json',
       processData: false,
-      contentType: false,
-      dataType: 'json'
+      contentType: false
     })
 
     .done(function(data) {
-      var html = sendMessage(data);
-      $('.chat-message').append(html);
+      console.log(data)
+      var html = sendMessage(data.message);
+      $('.chat-message').html(html);
+      $('#message_text').val("");
+      moveToBottom();
+
     })
 
     .fail(function() {
       alert('エラーが発生しました');
     });
   });
+
+  if(window.location.href.match(/messages/)) {
+    setInterval(function(){
+      var indexUrl = window.location.href + ".json";
+      $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: indexUrl
+      })
+
+      .done(function(data) {
+        var number = data.length;
+        var insertHtml = ""
+        for (var i = 0; i < number; i++)
+        {
+          insertHtml += sendMessage(data[i]);
+        };
+        $('.chat-messages').html(insertHtml);
+      })
+    },5 * 1000);
+  };
 });

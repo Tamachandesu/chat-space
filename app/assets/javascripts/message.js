2664779b1c2a7dd5f4cc77bdaecbfbc546381723
Tamachandesu <taturo.0324@gmail.com>
2017-02-28 22:41:22 +0900

    Fix message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,23 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+
   function sendMessage(message) {
     var buildImage = "";
 
     if(message.image) {
       buildImage = `<img class="chat-message__image" src="${message.image}" >`;
     }
-    var html =`
-            <li class="chat-message">
+    var buildHtml =
+            `<li class="chat-message">
               <div class="chat-message__header clearfix">
                 <p class="chat-message__name">${message.nickname}</p>
                 <p class="chat-message__time">${message.created_at}</p>
               </div>
               <p class = "chat-message__body">${message.text}<br></p>${buildImage}
             </li>`;
-    return html;
+    return buildHtml;
   }
 
-　function moveToBottom() {
+  function moveToBottom() {
     $('.chat-body').animate({
       scrollTop: $('.chat-messages').height() + $('.chat-message').height()
     })
@@ -29,7 +30,7 @@ $(function() {
 
     $.ajax({
       type: 'POST',
-      url: './messages.json',
+      url: 'messages',
       data: formData,
       dataType: 'json',
       processData: false,
@@ -37,12 +38,10 @@ $(function() {
     })
 
     .done(function(data) {
-      console.log(data)
       var html = sendMessage(data.message);
-      $('.chat-message').html(html);
-      $('#message_text').val("");
+      $('.chat-messages').append(html);
+      $('#message_text').val('');
       moveToBottom();
-
     })
 
     .fail(function() {
@@ -52,22 +51,21 @@ $(function() {
 
   if(window.location.href.match(/messages/)) {
     setInterval(function(){
-      var indexUrl = window.location.href + ".json";
       $.ajax({
         type: 'GET',
         dataType: 'json',
-        url: indexUrl
+        url: 'messages'
       })
 
       .done(function(data) {
         var number = data.length;
-        var insertHtml = ""
+        var insertHtml = "";
         for (var i = 0; i < number; i++)
         {
           insertHtml += sendMessage(data[i]);
         };
-        $('.chat-messages').html(insertHtml);
+          $('.chat-messages').html(insertHtml);
       })
-    },5 * 1000);
+    },10 * 1000);
   };
 });
